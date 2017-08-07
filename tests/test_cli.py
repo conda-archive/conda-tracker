efc052b6431fdc329e86de133c6b193ec9741e6d
mandeep <mbhutani@continuum.io>
2017-08-07 14:38:33 -0500

    Add assertion for omitted repos

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,11 +13,15 @@ def test_cli_workflow():
 
         assert 'my_agg_repo' in os.listdir()
 
-        runner.invoke(cli, ['add', 'conda', 'my_agg_repo'])
+        runner.invoke(cli, ['add', 'conda', 'my_agg_repo', '--omit=^conda.*$'])
 
-        conda_repos = ['conda', 'conda-build', 'conda-docs', 'conda-ui']
+        conda_omitted_repos = ['conda', 'conda-build', 'conda-docs', 'conda-ui']
+        conda_wanted_repos = ['constructor', 'cookiecutter-conda-python', 'ci_test',
+                              'libconda', 'kapsel', 'site', 'build_infrastructure',
+                              'PyCommunity']
 
-        assert all(repository in os.listdir('my_agg_repo') for repository in conda_repos)
+        assert all(repository not in os.listdir('my_agg_repo') for repository in conda_omitted_repos)
+        assert all(repository in os.listdir('my_agg_repo') for repository in conda_wanted_repos)
 
         update = runner.invoke(cli, ['update', 'my_agg_repo'])
         
