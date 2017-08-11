7b111e5aa33cd7aafe6ad0e23ae1343ccf74dcfc
mandeep <mbhutani@continuum.io>
2017-08-11 10:14:57 -0500

    Add install requirements of GitPython and PyGithub

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 setup(name='conda-tracker',
-      version='0.1.0',
+      version='0.1.1',
       author='Continuum Analytics',
       author_email='conda@continuum.io',
       description='Manage conda recipes',
       packages=['conda_tracker'],
       install_requires=[
-        'click==6.7',
+        'click>=6.7',
+        'GitPython>=2.1'
+        'PyGithub>=1.3',
       ],
       entry_points='''
         [console_scripts]
